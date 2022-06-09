d914456a4cff3941cb440f6df28ba220db774e62
Mina <minallkamel@gmail.com>
2022-06-09 13:01:36 +0200

    Breakup nodes and the parser into separate files

--- Makefile
+++ Makefile
@@ -1,3 +1,3 @@
-sad: lexer.d sad.d parser.d error.d util.d
+sad: lexer.d sad.d parser.d error.d util.d astnode.d
 	mkdir -p build
 	dmd $^ -of=build/$@
---
+++ astnode.d
@@ -0,0 +1,163 @@
+module astnode;
+
+import std.variant;
+import std.conv;
+
+abstract class AstNode {
+
+    static class PrimaryNode : AstNode
+    {
+        Variant mVal;
+
+        this(Variant val) {
+            mVal = val;
+        }
+
+        override string toString() {
+            return "Primary(" ~ mVal.toString() ~ ")";
+        }
+
+    }
+
+    static class NumberNode : PrimaryNode
+    {
+        this(Variant val) {
+            super(val);
+        }
+
+        override string toString() {
+            return "Number(" ~ mVal.toString() ~ ")";
+        }
+    }
+
+    static class IdentifierNode : PrimaryNode
+    {
+        this(Variant val) {
+            super(val);
+        }
+
+        override string toString() {
+            return "Identifier(" ~ mVal.toString() ~ ")";
+        }
+    }
+
+    static class BinaryNode : AstNode
+    {
+        string mOp;
+        AstNode mLhs;
+        AstNode mRhs;
+
+        this(string op, AstNode lhs, AstNode rhs) {
+            mOp = op;
+            mLhs = lhs;
+            mRhs = rhs;
+        }
+
+        override string toString() {
+            return "Binary(op: '" ~ mOp ~ "'" ~ ", lhs: " ~ mLhs.toString() ~ ", rhs: " ~ mRhs.toString() ~ ")";
+        }
+
+    }
+
+    
+    static class PrefixNode : AstNode
+    {
+        string mOp;
+        AstNode mRhs;
+
+        this (string op, AstNode rhs) {
+            mOp = op;
+            mRhs = rhs;
+        }
+
+        override string toString() {
+            return "Prefix(op: '" ~ mOp ~ "', rhs: " ~ mRhs.toString() ~ ")";
+        }
+    }
+
+    
+    static class LetDefinitionNode : AstNode
+    {
+        AstNode mIdentifier;
+        AstNode mRhs;
+
+        this (AstNode identifier, AstNode rhs) {
+            mIdentifier = identifier;
+            mRhs = rhs;
+        }
+
+        override string toString() {
+            return "Let(identifier: " ~ mIdentifier.toString() ~  ", rhs: " ~ mRhs.toString() ~ ")";
+        }
+    }
+
+    static class LetDeclarationNode : AstNode
+    {
+        AstNode mIdentifier;
+
+        this (AstNode identifier) {
+            mIdentifier = identifier;
+        }
+
+        override string toString() {
+            return "Let(identifier: " ~ mIdentifier.toString() ~ ")";
+        }
+    }
+
+    static class ConstDefinitionNode : AstNode
+    {
+        AstNode mIdentifier;
+        AstNode mRhs;
+
+        this (AstNode identifier, AstNode rhs) {
+            mIdentifier = identifier;
+            mRhs = rhs;
+        }
+
+        override string toString() {
+            return "Const(identifier: " ~ mIdentifier.toString() ~ ", rhs: " ~ mRhs.toString() ~ ")";
+        }
+    }
+
+    static class BlockNode : AstNode
+    {
+        AstNode[] mSubtree;
+
+        this (AstNode[] subtree) {
+            mSubtree = subtree;
+        }
+
+        override string toString() {
+            return "Block(" ~ to!string(mSubtree) ~ ")";
+        }
+    }
+
+    static class ParanNode : BlockNode
+    {
+        this (AstNode[] subtree) {
+            super(subtree);
+        }
+
+        override string toString() {
+            return "Paren(" ~ to!string(mSubtree) ~ ")";
+        }
+    }
+
+    static class FunctionNode : AstNode
+    {
+        AstNode mIdentifier;
+        AstNode mParams;
+        AstNode mBlock;
+
+        this (AstNode identifier, AstNode params, AstNode block) {
+            mIdentifier = identifier;
+            mParams = params;
+            mBlock = block;
+        }
+
+        override string toString() {
+            return "Fun(identifier: " ~ mIdentifier.toString() ~ ", params: "
+                ~ to!string(mParams) ~ ", block: " ~ mBlock.toString() ~ ")";
+        }
+    }
+}
