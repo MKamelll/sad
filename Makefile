f5ef7e4c2ca20c5f67e37fcb080a28cb5503ab85
Mina Kamel <minallkamel@gmail.com>
2022-07-15 22:25:40 +0200

    write the output to a file

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ SRC=./src
 AST=$(SRC)/ast
 COMPILER=$(SRC)/compiler
 
-sad: $(AST)/*.d $(COMPILER)/*.d $(SRC)/sad.d
+sad: $(AST)/*.d $(COMPILER)/*.d $(SRC)/*.d
 	mkdir -p build
 	dmd $^ -of=build/$@
--- src/sad.d
+++ src/sad.d
@@ -3,11 +3,13 @@ module sad;
 import ast.lexer;
 import ast.parser;
 import ast.error;
+import error;
 import compiler.transpiler;
 
 import std.stdio;
 import std.algorithm;
 import std.string : strip;
+import std.file;
 
 void run(string src) {
     auto lexer = new Tokenizer(src);
@@ -16,7 +18,48 @@ void run(string src) {
     writeln(transpiler.generate());
 }
 
-int main() {
+void generateDFile(string src, string fileName) {
+    auto lexer = new Tokenizer(src);
+    auto ast = new Ast(lexer);
+    auto transpiler = new Transpiler(ast.parse());
+    auto file = new File(fileName[0..$-4] ~ ".d", "w");
+    file.write(transpiler.generate());
+}
+
+string readFile(string fileName) {
+    string result = "";
+    try
+    {
+        result ~= readText(fileName);
+
+    } catch (Exception ex) {
+        writeln(ex.toString());
+    }
+    
+    return result;
+}
+
+string getStem(string filename) {
+    string result;
+    foreach_reverse (ch; filename) {
+        if (ch == '.') break;
+        result ~= ch;
+    }
+
+    return result.dup().reverse();
+}
+
+int main(string[] argv) {
+
+    if (argv.length > 1) {
+        string fileName = argv[1];
+        if (getStem(fileName) != "sad") {
+            throw new SadError("Expected a file with the extenstion '.sad'");
+        }
+        string src = readFile(fileName);
+        generateDFile(src, fileName);
+        return 0;
+    }
 
     writeln("Welcome to Sad..");
     while (true) {
